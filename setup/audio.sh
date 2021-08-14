30baec19b8c41b4ece8f62d71e2b53d175e0dd66
kirksaunders <kirksaunders12@gmail.com>
2021-08-14 02:30:46 -0400

    Added fiio udev rule

--- setup/audio.sh
+++ setup/audio.sh
@@ -7,4 +7,10 @@ echo "Starting audio setup..."
 echo "Installing pulseaudio..."
 sudo pacman -S pulseaudio pavucontrol
 
+echo "Installing Fiio Q3 udev rule..."
+sudo cp extra/fiio_q3.rules /etc/udev/rules.d/
+
+echo "Reloading udev rules..."
+sudo udevadm control --reload
+
 echo "Done with audio setup!"
