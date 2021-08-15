d4e24f8d6408ef1992f7c9135863cf382d0cd57b
kirksaunders <kirksaunders12@gmail.com>
2021-08-14 22:19:01 -0400

    Fixed more issues with theme setup script

--- setup/theme.sh
+++ setup/theme.sh
@@ -9,7 +9,8 @@ sudo pacman -S gtk3
 
 echo "Downloading Juno GTK theme..."
 mkdir -p ~/.themes
-git clone https://github.com/EliverLara/Juno.git ~/.themes/
+git clone https://github.com/EliverLara/Juno.git ~/.themes/Juno
+rm -rf ~/.themes/Juno/.git
 
 echo "Installing GTK config..."
 cp -r config/gtk-3.0 ~/.config/
