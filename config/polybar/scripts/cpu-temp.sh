e0cb4f2f77141dfc46040c929e78444867a04cef
kirksaunders <kirksaunders12@gmail.com>
2021-09-13 23:22:33 -0400

    Fixed various issues, especially with mouse

--- config/polybar/scripts/cpu-temp.sh
+++ config/polybar/scripts/cpu-temp.sh
@@ -4,7 +4,7 @@ CORE="${1}"
 OUTPUT=$(sensors -Au)
 
 IFS=$'\n'
-DATA=$(echo "${OUTPUT}" | egrep -A4 "${CORE}")
+DATA=$(echo "${OUTPUT}" | egrep -A2 "${CORE}")
 CURRENT=$(echo "${DATA}" | grep -Po "(?<=_input: )([0-9]+)")
 #HIGH=$(echo "${DATA}" | grep -Po "(?<=_max: )([0-9]+)")
 #CRIT=$(echo "${DATA}" | grep -Po "(?<=_crit: )([0-9]+)")
--- setup/mouse.sh
+++ setup/mouse.sh
@@ -4,16 +4,10 @@ set -e
 
 echo "Starting mouse setup..."
 
-echo "Installing mouse sens script..."
-sudo cp extra/mousesens.sh /usr/local/bin/
+echo "Installing mouse sens config..."
+sudo cp extra/50-mouse-sens.conf /etc/X11/xorg.conf.d/
 
-echo "Installing mouse sens udev rule..."
-sudo cp extra/mousesens.rules /etc/udev/rules.d/
-
-echo "Reloading udev rules..."
-sudo udevadm control --reload
-
-echo "Installing config to disable mouse acceleration..."
+echo "Installing mouse acceleration config..."
 sudo cp extra/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/
 
 echo "Done with mouse setup!"
