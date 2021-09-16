98bb381935961c77d8730a5a8e5a0b1077284e7e
kirksaunders <kirksaunders12@gmail.com>
2021-09-15 20:51:50 -0400

    Fixed some issues with plexamp

--- extra/plexamp.sh
+++ extra/plexamp.sh
@@ -7,6 +7,14 @@ then
     i3-msg "exec --no-startup-id /usr/bin/flatpak run --branch=stable --arch=x86_64 --command=plexamp-run --file-forwarding com.plexamp.Plexamp"
 else
     workspace=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true).name')
-    xdotool windowmap $id
-    i3-msg "[id=$id] move to workspace number $workspace"
+    workspace2=$(i3-msg -t get_tree | jq -r '.nodes[].nodes[].nodes[] | select(.floating_nodes[].nodes[].name=="Plexamp") | .name')
+    if [[ "$1" = "toggle" ]] && [[ "$workspace" = "$workspace2" ]]
+    then
+        xdotool windowunmap $id
+    else
+        xdotool windowmap $id
+        #i3-msg "[id=$id] move to workspace number $workspace"
+        i3-msg "[id=$id] move position mouse"
+        i3-msg "[id=$id] focus"
+    fi
 fi
