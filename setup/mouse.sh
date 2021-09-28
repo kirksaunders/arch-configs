8287421abc168dd8c512fda86f1ed12d2fabd842
kirksaunders <kirksaunders12@gmail.com>
2021-09-28 03:00:31 -0400

    Simplify mouse sens config

--- setup/mouse.sh
+++ setup/mouse.sh
@@ -4,9 +4,6 @@ set -e
 
 echo "Starting mouse setup..."
 
-echo "Installing mouse sens config..."
-sudo cp extra/50-mouse-sens.conf /etc/X11/xorg.conf.d/
-
 echo "Installing mouse acceleration config..."
 sudo cp extra/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/
 
