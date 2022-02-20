5dae6f330afea42f75bbd9476364b522deac3da1
kirksaunders <kirksaunders12@gmail.com>
2022-02-20 15:32:24 -0500

    Added support for nomachine remote

--- config/polybar/scripts/launch.sh
+++ config/polybar/scripts/launch.sh
@@ -6,15 +6,16 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
 
-monitor=$(xrandr | grep HDMI-0)
-
-echo $monitor
+rdp=$(xrandr | grep rdp0)
+nomachine=$(xrandr | grep nxoutput0)
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-if [ -z "$monitor" ];
+if [[ ! -z "$rdp" ]];
 then
     polybar xrdp &
-
+elif [[ ! -z "$nomachine" ]];
+then
+    polybar nomachine &
 else
     polybar left &
     polybar right &
