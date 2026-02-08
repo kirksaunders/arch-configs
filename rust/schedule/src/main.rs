00131b36f82f726a75e94a216aef3f40265be61f
kirksaunders <kirksaunders12@gmail.com>
2026-02-07 23:39:34 -0500

    Rewrite schedule utility with tokio

--- rust/schedule/src/main.rs
+++ rust/schedule/src/main.rs
@@ -1,17 +1,14 @@
 use std::ffi::OsStr;
-use std::io::Write;
-use std::process::{Child, Command, Stdio};
+use std::process::Stdio;
 use std::sync::Arc;
 use std::time::Duration;
 
-use futures::{StreamExt, join};
-
-use async_std::io::{BufReader, stdin};
-use async_std::io::prelude::BufReadExt;
-use async_std::sync::Mutex;
-use async_std::task::{sleep, spawn};
-
 use structopt::StructOpt;
+use tokio::io::{stdin, AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::process::{Child, Command};
+use tokio::sync::Mutex;
+use tokio::time::MissedTickBehavior;
+use tokio::{join, time};
 
 #[derive(StructOpt, Debug)]
 struct CLI {
@@ -21,90 +18,96 @@ struct CLI {
 
     /// Arguments to pass to the command.
     #[structopt(subcommand)]
-    command: SubCommand
+    command: SubCommand,
 }
 
 #[derive(StructOpt, Debug)]
 enum SubCommand {
     /// Command to run with its arguments.
     #[structopt(external_subcommand)]
-    Sub(Vec<String>)
+    Sub(Vec<String>),
 }
 
 impl SubCommand {
     #[allow(dead_code)]
     fn get(self) -> Vec<String> {
         match self {
-            SubCommand::Sub(v) => v
+            SubCommand::Sub(v) => v,
         }
     }
 
     fn as_ref(&self) -> &Vec<String> {
         match self {
-            SubCommand::Sub(v) => v
+            SubCommand::Sub(v) => v,
         }
     }
 
     #[allow(dead_code)]
     fn as_mut(&mut self) -> &Vec<String> {
         match self {
-            SubCommand::Sub(v) => v
+            SubCommand::Sub(v) => v,
         }
     }
 }
 
 struct ChildData {
-    proc: Option<Child>
+    proc: Option<Child>,
 }
 
 async fn run(command: &[impl AsRef<OsStr>], data: Arc<Mutex<ChildData>>, input: impl AsRef<str>) {
     let mut data = data.lock().await;
-    
+
     if let Some(child) = &mut data.proc {
         // Wait for last run to finish before running again
-        let _ = child.wait();
+        let _ = child.wait().await;
     }
 
-    data.proc = Some(Command::new(command[0].as_ref())
+    let mut proc = Command::new(command[0].as_ref())
         .args(command.iter().skip(1))
         .stdin(Stdio::piped())
         .spawn()
-        .expect("Unable to spawn child process"));
+        .expect("Unable to spawn child process");
 
-    data.proc.as_mut().unwrap().stdin.take().unwrap()
+    let result = proc
+        .stdin
+        .take()
+        .unwrap()
         .write_all(input.as_ref().as_bytes())
-        .expect("Unable to write to child process's stdin");
+        .await;
+    if let Err(e) = result {
+        eprintln!("Failed to write to child process stdin: {:?}", e);
+    }
+
+    data.proc = Some(proc);
 }
 
 async fn timer_loop(cli: &CLI, data: Arc<Mutex<ChildData>>) {
     match cli.interval {
-        Some(interval) => loop {
-            let t = spawn(sleep(Duration::from_secs_f64(interval)));
-            run(cli.command.as_ref(), data.clone(), "").await;
-
-            t.await;
-        },
-        None => run(cli.command.as_ref(), data.clone(), "").await
+        Some(interval) => {
+            let mut ticker = time::interval(Duration::from_secs_f64(interval));
+            ticker.set_missed_tick_behavior(MissedTickBehavior::Skip);
+            loop {
+                ticker.tick().await;
+                run(cli.command.as_ref(), data.clone(), "").await;
+            }
+        }
+        None => run(cli.command.as_ref(), data.clone(), "").await,
     }
 }
 
 async fn read_write_loop(cli: &CLI, data: Arc<Mutex<ChildData>>) {
     let mut lines = BufReader::new(stdin()).lines();
-    while let Some(line) = lines.next().await {
-        let mut line = line.expect("Unable to read from stdin");
+    while let Some(mut line) = lines.next_line().await.expect("Unable to read from stdin") {
         line.push('\n');
 
         run(cli.command.as_ref(), data.clone(), line).await;
     }
 }
 
-#[async_std::main]
+#[tokio::main]
 async fn main() {
     let cli = CLI::from_args();
-
-    let data = Arc::new(Mutex::new(ChildData {
-        proc: None
-    }));
+    let data = Arc::new(Mutex::new(ChildData { proc: None }));
 
     join!(timer_loop(&cli, data.clone()), read_write_loop(&cli, data));
 }
